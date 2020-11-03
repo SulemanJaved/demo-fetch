152523c1e48ef47bf8ece584abdd8804effe9a0b
Suleman Javed <sulemanjaved20@gmail.com>
2020-11-04 01:02:26 +0500

    Added material UI code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { AppBar, Button, Grid, Icon, IconButton, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+      flexGrow: 1,
+  },
+  testButton: {
+    backgroundColor: "red",
+  }
+}));
+
+
 
 function App() {
+  const classes =  useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      Hello World
+      <Button variant="contained" color="secondary">Hello Button</Button>
+      <div>
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.button}
+        endIcon={<Icon>send</Icon>}
+      >
+        Send
+      </Button>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          Hello  World 1
+        </Grid>
+        <Grid item xs={2} style={{backgroundColor:"red"}}>
+          Hello  World 2
+        </Grid>
+        <Grid item xs={8} style={{backgroundColor:"green"}}>h
+          Hello  World 3
+        </Grid>
+        <Grid item xs={2} style={{backgroundColor:"Blue"}}>
+          Hello  World 4
+        </Grid>
+        <Grid item xs={2}>
+          Hello  World 5  
+        </Grid>
+      </Grid>
+      <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit" className={classes.testButton}>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+      </div>
     </div>
   );
 }
