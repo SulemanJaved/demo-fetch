bdfe1cade64d2a1c1a07825034e3e94e3eac10d1
Suleman Javed <sulemanjaved20@gmail.com>
2020-11-04 21:53:24 +0500

    Added react chart-2 code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Bar} from 'react-chartjs-2';
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: 'rgba(0,99,132,0.2)',
+      borderColor: 'rgba(0,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(0,99,132,0.4)',
+      hoverBorderColor: 'rgba(0,99,132,1)',
+      data: [32, 59, 80, 81, 56, 55, 99]
+    }
+  ]
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h2>Bar Example (custom size)</h2>
+        <Bar
+          data={data}
+          width={100}
+          height={170}
+          options={{
+            maintainAspectRatio: false
+          }}
+        /> 
     </div>
   );
 }
